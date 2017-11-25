ad6fb32e4d178b310a20524added5bc58b12a822
react <react@xtz.com>
2017-11-25 21:28:50 +0530

    server port changed

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var _express2 = _interopRequireDefault(_express);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+app.set('port', process.env.PORT || 5000);
 app.get('/', function (req, res) {
 	res.sendFile('./index.html', { root: __dirname });
 });
 app.use(_express2.default.static(__dirname));
-app.listen(3000);
+//app.listen(3000);
